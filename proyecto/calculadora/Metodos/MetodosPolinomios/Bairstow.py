5cd83032685e75ebae289ee59c93dbef6933461e
Aracelii <mquispeyn@unsa.edu.pe>
2020-12-21 11:01:05 -0500

    Actualización 2.0

--- proyecto/calculadora/Metodos/MetodosPolinomios/Bairstow.py
+++ proyecto/calculadora/Metodos/MetodosPolinomios/Bairstow.py
@@ -1,70 +1,66 @@
-from . import conversor as cn
-import sympy
-import math; 
-
-MAX_ITERATIONS = 10000; 
-
-# Function to calculate f(x) 
-def f(x): 
-
-	# Taking f(x) = x ^ 3 + 2x ^ 2 + 10x - 20 
-	return (1 * pow(x, 3) + 2 * x * x +
-						10 * x - 20); 
-
-def RMuller(a, b, c): 
-
-	res = 0; 
-	i = 0; 
-
-	while (True): 
+import math
+import random
+'''
+   Bairstow's Method where:
+      r = Initial guess
+      s = Initial guess
+     roots = Empty Array
+   a = Coefficient's vector
+   g = Polinomial's degree   
+'''   
+def Rbairstow(a,r,s,g,roots):
 	
-		# Calculating various constants 
-		# required to calculate x3 
-		f1 = f(a); f2 = f(b); f3 = f(c); 
-		d1 = f1 - f3; 
-		d2 = f2 - f3; 
-		h1 = a - c; 
-		h2 = b - c; 
-		a0 = f3; 
-		a1 = (((d2 * pow(h1, 2)) -
-			(d1 * pow(h2, 2))) /
-			((h1 * h2) * (h1 - h2))); 
-		a2 = (((d1 * h2) - (d2 * h1)) /
-			((h1 * h2) * (h1 - h2))); 
-		x = ((-2 * a0) / (a1 +
-			abs(math.sqrt(a1 * a1 - 4 * a0 * a2)))); 
-		y = ((-2 * a0) / (a1 -
-			abs(math.sqrt(a1 * a1 - 4 * a0 * a2)))); 
+	if(g<1):
+		return 0
+	if((g==1) and (a[1]!=0)):
+		roots.append(float(-a[0])/float(a[1]))
+		return None
+	if(g==2):
+		D = (a[1]*2.0)-(4.0)*(a[2])*(a[0])
+		X1 = (-a[1] - math.sqrt(D))/(2.0*a[2])
+		X2 = (-a[1] + math.sqrt(D))/(2.0*a[2])
+		roots.append(X1)
+		roots.append(X2)
+		return 0
+	n = len(a)
+	b = [0]*len(a)
+	c = [0]*len(a)
+	b[n-1] = a[n-1]
+	b[n-2] = a[n-2] + r*b[n-1]
+	i = n - 3
+	while(i>=0):
+		b[i] = a[i] + r*b[i+1] + s*b[i+2]
+		i = i - 1
+	c[n-1] = b[n-1]
+	c[n-2] = b[n-2] + r*c[n-1]
+	i = n - 3
+	while(i>=0):
+		c[i] = b[i] + r*c[i+1] + s*c[i+2]
+		i = i - 1
+	Din = ((c[2]*c[2])-(c[3]*c[1]))*(-1.0)
+	print("ra ",Din)
+	r = r + (Din)*((c[2])*(-b[1])+(-c[3])*(-b[0]))
+	s = s + (Din)*((-c[1])*(-b[1])+(c[2])*(-b[0]))
+	if(abs(b[0])>1E-14 or abs(b[1])>1E-14):
+		return Rbairstow(a,r,s,g,roots)
+	if (g>=3):
+		Dis = ((-r)*(2.0))-((4.0)*(1.0)*(-s))
+		X1 = (r - (math.sqrt(Dis)))/(2.0)
+		X2 = (r + (math.sqrt(Dis)))/(2.0)
+		roots.append(X1)
+		
+		roots.append(X2)
+		return Rbairstow(b[2:],r,s,g-2,roots)	
 
-		# Taking the root which is 
-		# closer to x2 
-		if (x >= y): 
-			res = x + c; 
-		else: 
-			res = y + c; 
 
-		# checking for resemblance of x3 
-		# with x2 till two decimal places 
-		m = res * 100; 
-		n = c * 100; 
-		m = math.floor(m); 
-		n = math.floor(n); 
-		if (m == n): 
-			break; 
-		a = b; 
-		b = c; 
-		c = res; 
-		if (i > MAX_ITERATIONS): 
-			print("Root cannot be found using", 
-							"Muller's method"); 
-			break; 
-		i += 1; 
-	if (i <= MAX_ITERATIONS): 
-		print("The value of the root is", 
-						round(res, 4)); 
-
-# Driver Code 
-a = 0; 
-b = 1; 
-c = 2; 
-RMuller(a, b, c); 
+k = 0
+g = 5
+roots = []
+a = [1,-3.5,2.75,2.125,-3.875,1.25]
+r = -1
+s = -1
+Rbairstow(a,r,s,g,roots)
+print("\nFound Roots => \n")
+for ra in roots:
+	print("R ",ra )
+	k += 1
